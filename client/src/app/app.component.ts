27dbbdcb41dec54f94b7ddf90005a1ed2a71af1d
dMundov <mundov@abv.bg>
2021-10-17 23:17:11 +0300

    InitialCreateClientApp

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'DateME';
+  users: any;
+
+  constructor(private http: HttpClient, private accountService: AccountService) { }
+
+  ngOnInit() {
+    this.getUsers();
+    this.setCurrentUser();
+  }
+
+
+  setCurrentUser() {
+    const user: User = JSON.parse(localStorage.getItem('user')!);
+    this.accountService.setCurrentUser(user)
+  }
+  getUsers() {
+    this.http.get('https://localhost:5001/api/users').subscribe(response => {
+      this.users = response;
+    }, error => {
+      console.log(error);
+    })
+  }
+}
+
+
