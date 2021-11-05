a371a395cddfb853123c00fddae4e382a505dce2
dMundov <mundov@abv.bg>
2021-11-06 01:08:19 +0200

    MemberCardsAdded, MemberProfilePageAdde, MemberGalleryAdded

---
+++ client/src/app/_models/member.ts
@@ -0,0 +1,20 @@
+import { Photo } from "./Photo";
+
+export interface Member {
+    id: string;
+    username: string;
+    photoUrl: string;
+    age: number;
+    knownAs: string;
+    createdOn: Date;
+    lastActive: Date;
+    gender: string;
+    introduction: string;
+    lookingFor: string;
+    interests: string;
+    city: string;
+    country: string;
+    photos: Photo[];
+}
+
+
--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -18,14 +18,14 @@ const routes: Routes = [
     canActivate: [AuthGuard],
     children: [
       { path: 'members', component: MemberListComponent, canActivate: [AuthGuard] },
-      { path: 'members/:id', component: MemberDetailComponent },
+      { path: 'members/:username', component: MemberDetailComponent },
       { path: 'lists', component: ListsComponent },
       { path: 'messages', component: MessagesComponent },
     ]
   },
-  {path:'errors', component: TestErrorsComponent},
-  {path:'not-found',component:NotFoundComponent},
-  {path:'server-error',component:ServerErrorComponent},
+  { path: 'errors', component: TestErrorsComponent },
+  { path: 'not-found', component: NotFoundComponent },
+  { path: 'server-error', component: ServerErrorComponent },
   { path: '**', component: NotFoundComponent, pathMatch: 'full' }
 
 ];
