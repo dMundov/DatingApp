138de818861509680c6137920602e2649a2902fa
Dimitar Mundov <mundofff@gmail.com>
2022-11-19 23:58:13 +0200

    SettingUpClientPagination

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,9 +1,10 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { of } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
+import { PaginatedResult } from '../_models/pagination';
 
 
 
@@ -14,15 +15,26 @@ export class MembersService {
 
   baseUrl = environment.apiUrl;
   members: Member[] = [];
+  padinatedResult: PaginatedResult<Member[]> = new PaginatedResult<Member[]>();
 
   constructor(private http: HttpClient) { }
 
-  getMembers() {
-    if (this.members.length > 0) return of(this.members)
-    return this.http.get<Member[]>(this.baseUrl + 'users').pipe(
-      map(members => {
-        this.members = members;
-        return members;
+  getMembers(page: number | null, itemsPerPage: number | null) {
+
+    let params = new HttpParams();
+
+    if (page !== null && itemsPerPage !== null) {
+      params = params.append('pageNumber', page.toString());
+      params = params.append('pageSize', itemsPerPage?.toString());
+    }
+    //if (this.members.length > 0) return of(this.members)
+    return this.http.get<Member[]>(this.baseUrl + 'users', {observe:'response', params}).pipe(
+      map(response=>{
+        this.padinatedResult.results = response.body as any;
+        if(response.headers.get('pagination') !== null){
+          this.padinatedResult.pagination = JSON.parse(response.headers.get('pagination') as any);
+        }
+        return this.padinatedResult;
       })
     )
   }
@@ -30,24 +42,24 @@ export class MembersService {
   getMember(username: string | null) {
     const member = this.members.find(x => x.username === username);
     if (member !== undefined) return of(member);
-      return this.http.get<Member>(this.baseUrl + 'users/' + username);
+    return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 
   updateMember(member: Member) {
     return this.http.put(this.baseUrl + 'users', member).pipe(
-      map(()=>{
+      map(() => {
         const index = this.members.indexOf(member);
-        this.members[index]=member;
+        this.members[index] = member;
       })
     )
 
   }
 
-  setMainPhoto(photoId:string){
-    return this.http.put(this.baseUrl + 'users/set-main-photo/' + photoId, {} );
+  setMainPhoto(photoId: string) {
+    return this.http.put(this.baseUrl + 'users/set-main-photo/' + photoId, {});
   }
 
-  deletePhoto(photoId:string){
-     return this.http.delete(this.baseUrl + 'users/delete-photo/'+ photoId);
+  deletePhoto(photoId: string) {
+    return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId);
   }
 }
