b248077fd5107ab3f9f452ff7e9acbb9c1ff77b1
Dimitar Mundov <mundofff@gmail.com>
2022-12-10 02:46:56 +0200

    Admin Panel Updated, Roles management modal added

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -31,6 +31,9 @@ import { DateInputComponent } from './_forms/date-input/date-input.component';
 import { MemberMessagesComponent } from './_members/member-messages/member-messages.component';
 import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
 import { HasRoleDirective } from './_directives/has-role.directive';
+import { UserManagementComponent } from './admin/user-management/user-management.component';
+import { PhotoManagementComponent } from './admin/photo-management/photo-management.component';
+import { RolesModalComponent } from './modals/roles-modal/roles-modal.component';
 
 @NgModule({
   declarations: [
@@ -52,7 +55,10 @@ import { HasRoleDirective } from './_directives/has-role.directive';
     DateInputComponent,
     MemberMessagesComponent,
     AdminPanelComponent,
-    HasRoleDirective
+    HasRoleDirective,
+    UserManagementComponent,
+    PhotoManagementComponent,
+    RolesModalComponent
   ],
   imports: [
     BrowserModule,
