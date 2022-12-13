27d262a4d88aff419e83bc7c5db020dfd0024bbf
Dimitar Mundov <mundofff@gmail.com>
2022-12-13 23:52:18 +0200

    fix name convention communication bug from API to the client

--- API/DTos/UserDto.cs
+++ API/DTos/UserDto.cs
@@ -2,7 +2,7 @@ namespace API.DTos
 {
     public class UserDto
     {
-        public string UserName { get; set; }
+        public string Username { get; set; }
         public string Token { get; set; }
 
         public string PhotoUrl { get; set; }
