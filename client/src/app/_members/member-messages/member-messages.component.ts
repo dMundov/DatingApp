2388535a575703ebb17015bc3e4c6fd4e3f463b0
Dimitar Mundov <mundofff@gmail.com>
2022-12-20 00:57:35 +0200

    Added Scroll to Messages Tab

--- client/src/app/_members/member-messages/member-messages.component.ts
+++ client/src/app/_members/member-messages/member-messages.component.ts
@@ -1,9 +1,12 @@
-import { Component, Input, OnInit, ViewChild } from '@angular/core';
+
+import { ChangeDetectionStrategy, Component, Input, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Message } from 'src/app/_models/message';
 import { MessageService } from 'src/app/_services/message.service';
 
 @Component({
+  //fix console error for messages Tab from Angular
+  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-member-messages',
   templateUrl: './member-messages.component.html',
   styleUrls: ['./member-messages.component.css']
