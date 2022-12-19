0f643264d849c824007165b51b306f264cdf8ba7
Dimitar Mundov <mundofff@gmail.com>
2022-12-20 01:19:09 +0200

    Fix Home component Console Error for null user

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -14,15 +14,17 @@ export class AccountService {
   baseUrl = environment.apiUrl;
   private currentUserSource = new ReplaySubject<User>(1);
   currentUser$ = this.currentUserSource.asObservable();
+  isLoggedIn: boolean = false;
 
   constructor(private http: HttpClient, private presenceService: PresenceService) { }
 
-  login(model: User) {
+  login(model: User | null) {
     return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
       map((response: User) => {
         const user = response;
         if (user) {
           this.setCurrentUser(user);
+          this.isLoggedIn=true;
         }
       })
     )
@@ -40,13 +42,16 @@ export class AccountService {
 
   }
 
-  setCurrentUser(user: User) {
-    user.roles = [];
-    const roles = this.getDecodedToken(user.token).role;
-    Array.isArray(roles) ? user.roles = roles : user.roles.push(roles);
-    localStorage.setItem('user', JSON.stringify(user));
-    this.currentUserSource.next(user);
-    this.presenceService.createHubConnection(user);
+  setCurrentUser(user: User ) {
+    if(this.isLoggedIn === true)
+    {
+      user.roles = [];
+      const roles = this.getDecodedToken(user.token).role;
+      Array.isArray(roles) ? user.roles = roles : user.roles.push(roles);
+      localStorage.setItem('user', JSON.stringify(user));
+      this.currentUserSource.next(user);
+      this.presenceService.createHubConnection(user);
+    }
   }
 
   logout() {
