bcb6d17b81152de723211f82cf3561f3902a8950
Dimitar Mundov <mundofff@gmail.com>
2023-01-02 02:07:36 +0200

    Fix reload logOut error with currentUser check Fix empty User.Roles in home page

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -14,7 +14,6 @@ export class AccountService {
   baseUrl = environment.apiUrl;
   private currentUserSource = new ReplaySubject<User>(1);
   currentUser$ = this.currentUserSource.asObservable();
-  isLoggedIn: boolean = false;
 
   constructor(private http: HttpClient, private presenceService: PresenceService) { }
 
@@ -23,7 +22,6 @@ export class AccountService {
       map((response: User) => {
         const user = response;
         if (user) {
-          this.isLoggedIn=true;
           this.setCurrentUser(user);
         }
       })
@@ -42,15 +40,14 @@ export class AccountService {
 
   }
 
-  setCurrentUser(user: User ) {
-    if(this.isLoggedIn === true)
-    {
+  setCurrentUser(user: User ) { 
+    if(user){
       user.roles = [];
       const roles = this.getDecodedToken(user.token).role;
       Array.isArray(roles) ? user.roles = roles : user.roles.push(roles);
       localStorage.setItem('user', JSON.stringify(user));
       this.currentUserSource.next(user);
-      this.presenceService.createHubConnection(user);
+      this.presenceService.createHubConnection(user); 
     }
   }
 
